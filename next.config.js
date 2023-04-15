af888490b07d9d97707bb11e07852d3a878ad907
Ilya <mailbox4me@ya.ru>
2023-04-15 19:56:04 +0300

    add ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -7,4 +7,11 @@ module.exports = {
     ],
     deviceSizes: [150, 250, 350, 640, 750, 828, 1080, 1200],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
